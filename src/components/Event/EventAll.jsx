e47e8c6512d39649d8c832785236968e57216752
PhuLookingGood <Boomza445533@gmail.com>
2025-08-05 10:55:20 +0700

    handle msgError

--- src/components/Event/EventAll.jsx
+++ src/components/Event/EventAll.jsx
@@ -12,7 +12,7 @@ import { get_events } from "@/api/event";
 
 const EventAll = () => {
   const [events, setEvents] = useState([]);
-  const [msgError, setMagError] = useState("")
+  const [msgError, setMagError] = useState("");
 
   useEffect(() => {
     handleGetEvents();
@@ -23,12 +23,12 @@ const EventAll = () => {
       const res = await get_events();
       setEvents(res.data);
     } catch (err) {
-      setMagError(err.response?.data?.detail || "Event fail")
-      console.log(err)
+      setMagError(err.response?.data?.detail || "Event fail");
+      console.log(err);
     }
   };
 
-  console.log(msgError)
+  console.log(msgError);
 
   console.log(events);
   return (
@@ -44,41 +44,44 @@ const EventAll = () => {
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {events.map((event, idx) => (
-            <Card
-              key={idx}
-              className="transition-all duration-300 transform hover:-translate-y-2 hover:shadow-2xl"
-            >
-              <div className="aspect-w-16 aspect-h-9">
-                <img
-                  src={event.image_cover}
-                  alt={event.title}
-                  className="w-full h-full object-cover"
-                  
-                />
-              </div>
-              <CardHeader>
-                <CardTitle>{event.title}</CardTitle>
-                <div className="mt-3 space-y-2 text-sm text-gray-600">
-                  <CardDescription className="flex items-center">
-                    <Calendar className="w-4 h-4 mr-2 text-gray-500" />
-                    {event.date}
-                  </CardDescription>
-                  <CardDescription className="flex items-center">
-                    <MapPin className="w-4 h-4 mr-2 text-gray-500" />
-                    {event.location}
-                  </CardDescription>
+          {msgError ? (
+            <h1 className="text-center text-red-500 col-span-full">
+              {msgError}
+            </h1>
+          ) : (
+            events.map((index,event) => (
+              <Card
+                key={index}
+                className="transition-all duration-300 transform hover:-translate-y-2 hover:shadow-2xl"
+              >
+                <div className="aspect-w-16 aspect-h-9">
+                  <img
+                    src={event.image_cover}
+                    alt={event.title}
+                    className="w-full h-full object-cover"
+                  />
                 </div>
-              </CardHeader>
-              <CardFooter>
-                <Button>View Photos</Button>
-              </CardFooter>
-            </Card>
-          ))}
+                <CardHeader>
+                  <CardTitle>{event.title}</CardTitle>
+                  <div className="mt-3 space-y-2 text-sm text-gray-600">
+                    <CardDescription className="flex items-center">
+                      <Calendar className="w-4 h-4 mr-2 text-gray-500" />
+                      {event.date}
+                    </CardDescription>
+                    <CardDescription className="flex items-center">
+                      <MapPin className="w-4 h-4 mr-2 text-gray-500" />
+                      {event.location}
+                    </CardDescription>
+                  </div>
+                </CardHeader>
+                <CardFooter>
+                  <Button>View Photos</Button>
+                </CardFooter>
+              </Card>
+            ))
+          )}
         </div>
-        
       </div>
-      <h1>{msgError}</h1>
     </section>
   );
 };
