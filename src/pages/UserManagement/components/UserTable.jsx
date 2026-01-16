8f3d2c46f3f6bbe66263b05c404452b2b90e5e85
unikZer0 <unik09john@gmail.com>
2026-01-16 11:05:05 +0700

    update showing role

--- src/pages/UserManagement/components/UserTable.jsx
+++ src/pages/UserManagement/components/UserTable.jsx
@@ -83,7 +83,7 @@ const UserTable = ({
                                                     user.role === 'admin' ? 'bg-blue-100 text-blue-800' :
                                                         user.role === 'user' ? 'bg-green-100 text-green-800' :
                                                             'bg-gray-100 text-gray-800'}`}>
-                                                {user.role}
+                                                {user.role === 'user' ? 'Photographer' : user.role}
                                             </span>
                                         </TableCell>
                                         <TableCell>
